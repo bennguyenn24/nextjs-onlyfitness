4679a3f659f74e3c7a5a48d5080163f03e9b4ec0
Ben Nguyen <bennguyenn24@gmail.com>
2023-06-07 20:04:08 -0700

    Refactored Navbar with Pages Configuration

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,30 @@
+import Link from 'next/link';
 import LogOutBtn from "./LogOutBtn";
+import { currentUser } from '@clerk/nextjs';
 
-export default function Navbar() {
+const Navbar = () => {
     return (
-        <nav>
+      <nav>
+        <ul className="flex items-center justify-between bg-gray-900 max-w-7xl sm:px-8 lg:px-8 h-16 ">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/trainers">Find Trainers</Link>
+          </li>
+          <li>
+            <Link href="/profile">Profile</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+          <li>
             <LogOutBtn />
-        </nav>
+          </li>
+        </ul>
+      </nav>
     );
-}
+  };
+  
+  export default Navbar;
+
